472b931412eac205d1c85ad56ae8b1cc4639524e
Huelsgoat <huntsperger@gmail.com>
2020-06-12 15:21:23 -0700

    more unicorns, starting on scroll page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Anim1 from "./components/Anim1"
 import Home from "./components/Home"
+import Scroll from "./components/Scroll"
+import { Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-        <Home></Home>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/scroll">
+            <Scroll/>
+          </Route>
+        </Switch>
+       
     </div>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 import "./style.css"
 import Unicorn from "./../../assets/unicorn.png"
 import Unicorn2 from "./../../assets/unicorn3.png"
 import Unicorn4 from "./../../assets/unicorn4.png"
 import Unicorn8 from "./../../assets/unicorn8.png"
 import Unicorn13 from "./../../assets/unicorn13.png"
+import Unicorn11 from "./../../assets/unicorn11.png"
+import Unicorn20 from "./../../assets/unicorn20.png"
+import Unicorn45 from "./../../assets/unicorn45.png"
 
 
 export default function Home() {
@@ -60,7 +64,7 @@ export default function Home() {
 
     }
     const uniPeek = {
-        
+
     }
 
 
@@ -70,13 +74,14 @@ export default function Home() {
             variants={mainVariants}
             initial="hidden"
             animate="visible"
+            size={100}
         >
             <motion.div
 
                 variants={childVariants}
                 whileHover={uniHover}
 
-                className="jumbotron jumbotron-fluid bg-red text-center">
+                className="jumbotron uniJumbo jumbotron-fluid bg-red text-center">
                 <img src={Unicorn2}></img>
             </motion.div>
 
@@ -85,7 +90,7 @@ export default function Home() {
                 variants={childVariantsRev}
                 whileHover={uniHover}
 
-                className="jumbotron jumbotron-fluid bg-orange text-center">
+                className="jumbotron uniJumbo jumbotron-fluid bg-orange text-center">
                 <img src={Unicorn2}></img>
 
             </motion.div>
@@ -95,7 +100,7 @@ export default function Home() {
                 variants={childVariants}
                 whileHover={uniHover}
 
-                className="jumbotron jumbotron-fluid bg-yellow text-center">
+                className="jumbotron uniJumbo jumbotron-fluid bg-yellow text-center">
                 <img src={Unicorn2}></img>
 
             </motion.div>
@@ -105,7 +110,7 @@ export default function Home() {
                 variants={childVariantsRev}
                 whileHover={uniHover}
 
-                className="jumbotron jumbotron-fluid bg-green text-center">
+                className="jumbotron uniJumbo jumbotron-fluid bg-green text-center">
                 <img src={Unicorn2}></img>
 
             </motion.div>
@@ -114,7 +119,7 @@ export default function Home() {
 
                 variants={childVariants}
                 whileHover={uniHover}
-                className="jumbotron jumbotron-fluid bg-blue text-center">
+                className="jumbotron uniJumbo jumbotron-fluid bg-blue text-center">
                 <img src={Unicorn2}></img>
 
             </motion.div>
@@ -123,75 +128,100 @@ export default function Home() {
 
                 variants={childVariantsRev}
                 whileHover={uniHover}
-                className="jumbotron jumbotron-fluid bg-violet text-center">
-                <motion.img 
+                className="jumbotron uniJumbo jumbotron-fluid bg-violet text-center">
+                <motion.img
                     src={Unicorn2}></motion.img>
 
             </motion.div>
             <motion.div
 
                 variants={childVariants}
-                className="bg-primary" id="unicorn">
+                className="bg-primary row" id="unicorn">
                 <motion.img
-                    initial={{ x: -1500 }}
-                    animate={{ x: 1500 }}
+                    className="col-6 unileftright1"
+                    initial={{ x: '-60vw' }}
+                    animate={{ x: '110vw' }}
                     transition={{ duration: 3, delay: 8, loop: "Infinity" }}
 
                     alt="unicorn"
                     src={Unicorn}
                 ></motion.img>
                 <motion.img
-                    initial={{ x: -1500 }}
-                    animate={{ x: 1500 }}
+                    className="col-6 unileftright"
+                    initial={{ x: '-100vw' }}
+                    animate={{ x: '115vw' }}
                     transition={{ duration: 3, delay: 9, loop: "Infinity" }}
 
                     alt="unicorn"
                     src={Unicorn4}
                 ></motion.img>
-                
-                
+
+
             </motion.div>
-            <motion.div
+            <div className="container-fluid">
+                <motion.div
+                    className="row"
+                    variants={childVariantsRev}
+                    id="ground">
 
-                variants={childVariantsRev}
-                 id="ground">
-                <h1>Unicorn Town</h1>
-                
-                 <motion.img
-                    initial={{ x: -600}}
-                    animate={{ x: -400 }}
-                    transition={{ duration: 3, delay: 9, yoyo: "Infinity" }}
+                    <motion.img
+                        className="col-6"
+                        initial={{ x: "-50vw" }}
+                        animate={{ x: "-10vw" }}
+                        transition={{ duration: 3, delay: 9, yoyo: "Infinity" }}
 
-                    alt="unicorn"
-                    src={Unicorn4}
-                ></motion.img>
-                 <motion.img
-                    initial={{ x: 1000}}
-                    animate={{ x: 800 }}
-                    transition={{ duration: 3, delay: 9, yoyo: "Infinity" }}
+                        alt="unicorn"
+                        src={Unicorn20}
+                    ></motion.img>
 
-                    alt="unicorn"
-                    src={Unicorn8}
-                ></motion.img>
-                 <motion.img
-                    id="bottomUni"
-                    initial={{ y: 370, x:-500}}
-                    animate={{ y: 100, x:-500}}
-                    transition={{ duration: 3, delay: 9, yoyo: "Infinity", }}
+                    {/* <motion.img
+                        className="col-3"
 
-                    alt="unicorn"
-                    src={Unicorn13}
-                ></motion.img>
-                 <motion.img
-                    id="bottomUni2"
-                    initial={{ y: -15, x:1100}}
-                    animate={{ y: -200, x:1100 }}
-                    transition={{ duration: 3, delay: 7, yoyo: "Infinity", }}
+                        id="bottomUni"
+                        initial={{ y: '100vh', x: 0 }}
+                        animate={{ y: '10vh', x:0 }}
+                        transition={{ duration: 3, delay: 9, yoyo: "Infinity", }}
 
-                    alt="unicorn"
-                    src={Unicorn13}
-                ></motion.img>
-                 
+                        alt="unicorn"
+                        src={Unicorn13}
+                    ></motion.img> */}
+
+                    <motion.img
+                        className="col-6"
+                        initial={{ x: "70vw" }}
+                        animate={{ x: "20vw" }}
+                        transition={{ duration: 3, delay: 9, yoyo: "Infinity" }}
+
+                        alt="unicorn"
+                        src={Unicorn11}
+                    ></motion.img>
+
+                </motion.div>
+            </div>
+            <motion.div className="jumbotron jumbotron-fluid bg-gradient-violet text-center"
+                style={{ marginBottom: 0 }}
+                initial={{ x: -1000, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 10 }}
+            >
+                <span><motion.img src={Unicorn45}
+                    initial={{ x: -10, y: 10, rotate: -30 }}
+                    animate={{ x: -10, y: -10, rotate: 30 }}
+                    transition={{ yoyo: Infinity }}
+                ></motion.img></span><Link to="/scroll"><motion.button
+
+                    className="btn-lg"
+                    whileHover={{
+                        scale: 1.3,
+                        transition: {
+                            yoyo: Infinity
+                        }
+                    }}
+                >MORE UNICORNS!!!!</motion.button></Link><span><motion.img src={Unicorn45}
+                    initial={{ x: 10, y: 10, rotate: 30 }}
+                    animate={{ x: 10, y: -10, rotate: -30 }}
+                    transition={{ yoyo: Infinity }}
+                ></motion.img></span>
             </motion.div>
         </motion.div>
     )
