70c1ca12b812121a26f7a1fee5403feec009861b
Huelsgoat <huntsperger@gmail.com>
2020-07-08 14:14:00 -0700

    more tidying

--- src/components/ImageBucket/index.js
+++ src/components/ImageBucket/index.js
@@ -2,16 +2,12 @@ import './style.css';
 import React from 'react';
 import { motion } from 'framer-motion'
 
-
-
 export default function ImageBucket({ src, ...style }) {
-    
-    
+
     return (
         <div className="ImageBucket">
-            <motion.img  src={src} style={{...style}} alt="unicorn" ></motion.img>
+            <motion.img src={src} style={{ ...style }} alt="unicorn" ></motion.img>
 
         </div>
-
     )
 }
